8c874794c325bd9b9044d97332274ce784a93b83
bekabadzagua <beka.badzagua@gmail.com>
2022-09-15 16:14:56 +0400

    refactor: format code & change names according to conventions

--- assests/scripts/contact.js
+++ assests/scripts/contact.js
@@ -1,36 +1,35 @@
 let beasts = [];
 let itemList = document.getElementById('items');
 
-function addModalDate(){
-    
-    const beastName = document.getElementById('input-name').value;
-    const beastPhone = document.getElementById('input-phone').value;
+function addModalDate() {
+  const beastName = document.getElementById('input-name').value;
+  const beastPhone = document.getElementById('input-phone').value;
 
-    if(beastName == "" || beastPhone == ""){
-        alert("შეიყვანეთ ტექსტი");
-    }else{
+  if (beastName === '' || beastPhone === '') {
+    alert('შეიყვანეთ ტექსტი');
+  } else {
+    beasts.push({
+      name: beastName,
+      phone: beastPhone
+    });
 
-        const newBeast = {
-            name: beastName,
-            phone: beastPhone
-        };
-        beasts.push(newBeast);
+    //აქ უნდა შევცვალო li ები button ებად
+    let contact = document.createElement('li');
+    contact.className = 'contact-list';
+    contact.innerText = `${beastName}  ${beastPhone}`;
 
-        //აქ უნდა შევცვალო li ები button ებად
-        let bttn = document.createElement('button');
-        bttn.className = 'contact-list';
-        bttn.appendChild(document.createTextNode(beastName + " " + beastPhone));
-        itemList.appendChild(bttn);
-
-
-        //alert(beasts[0].name + beasts.length);
-
-        // close ღილაკზე დაჭერისას რომ დაიხუროს modal ფანჯარა
-        document.getElementById("open-modal-window").style.display ="none";
-        //modal ფანჯრის დახურვის მერე გასუფთავდეს input 
-        document.getElementById('input-name').value = '';
-        document.getElementById('input-phone').value = '';
+    contact.onclick = () => {
+      console.log('hello world!');
     };
-};
 
+    itemList.appendChild(contact);
 
+    closeModal();
+  }
+}
+
+function closeModal() {
+  document.getElementById('open-modal-window').style.display = 'none';
+  document.getElementById('input-name').value = '';
+  document.getElementById('input-phone').value = '';
+}
--- assests/scripts/script.js
+++ assests/scripts/script.js
@@ -1,20 +1,20 @@
 var contactItem = document.getElementsByClassName('contact-list').textContent;
 
+console.log(contactItem);
 
-contactItem.addEventListener("click", openContact);
-
+contactItem.addEventListener('click', openContact);
 
 //modal ფანჯრის გახსნა
-function openModalWindow(){
-    document.getElementById("open-modal-window").style.display ="flex";
+function openModalWindow() {
+  document.getElementById('open-modal-window').style.display = 'flex';
 }
+
 //modal ფანჯრის დახურვა
-function closeModalWindow(){
-    document.getElementById("open-modal-window").style.display="none";
-}
-function openContact(e){
-    e.preventDefault();
-    alert("yes!");
+function closeModalWindow() {
+  document.getElementById('open-modal-window').style.display = 'none';
 }
 
-
+function openContact(e) {
+  e.preventDefault();
+  alert('yes!');
+}
